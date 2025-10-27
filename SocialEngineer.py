57f3209fc98699f8603fd8721dda39275489d183
Phil Hunt <philiphuntiv2@gmail.com>
2025-10-27 06:28:47 -0400

    Update SocialEngineer.py

--- SocialEngineer.py
+++ SocialEngineer.py
@@ -1,26 +1,6 @@
-#!/usr/bin/env python
-
 """
-SocialEngineer - Social Engineering Toolkit
--------------------------------------------
-
-Author      : Karthikeyan (https://karthithehacker.com)
-GitHub      : https://github.com/karthi-the-hacker
-Project     : SocialEngineer - An all-in-one CLI framework for social engineering
-
-License     : Open-source — strictly for educational and ethical hacking purposes ONLY.
-
-Note to Users:
---------------
-🔐 This tool is intended solely for educational use, research, and authorized security testing.
-🚫 Unauthorized use of this tool on networks you do not own or lack permission to test is illegal.
-❗ If you use or modify this code, PLEASE GIVE PROPER CREDIT to the original author.
-
-Warning to Code Thieves:
-------------------------
-❌ Removing this header or claiming this project as your own without credit is unethical and violates open-source principles.
-🧠 Writing your own code earns respect. Copy-pasting without attribution does not.
-✅ Be an ethical hacker. Respect developers' efforts and give credit where it’s due.
+Original Author    : https://github.com/karthi-the-hacker
+GitHub             : https://github.com/minefarts
 """
 
 import sys
